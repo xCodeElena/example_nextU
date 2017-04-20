1101bf16031843372c82803b8b0f3da81768a1bc
mac-uanl <mac-uanl@mac-uanls-iMac.local>
2017-04-20 16:38:32 -0500

    commit local

--- MiProyecto/ViewController.swift
+++ MiProyecto/ViewController.swift
@@ -10,16 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var nameField: UIView!
+    @IBOutlet var nameTextField: UIView!
+    @IBOutlet weak var labelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionButton(_ sender: Any) {
+        
+        labelName.text = "Hola )"
+   view.endEditing(true)
+    }
+    
 
 }
 
